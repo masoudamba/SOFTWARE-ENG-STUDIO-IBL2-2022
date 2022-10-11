93afb5e5ca46e8fb469ef4de125d01a579d91bf0
masoud <otienoamba81@gmail.com>
2022-10-12 00:32:37 +0300

    blank pages

---
+++ usermanagement.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Laundry Services</title> 
+  </head>
+  <div class="header">
+    <div class="logoWrapper">
+      <img class="logo" src="images/download.png" border="0" />
+      <div class="topnav">
+        <a href="usersignin.php">Log in</a>
+        <a href="userregister.php">Sign up</a>
+        <a href="usermanagement.php">Home</a>
+        <a href="welcome.php">Go back</a>
+      </div>
+    </div>
+    <div class="motto">
+      <h1>We offer the most conveninent and proffessional way to laundry.</h1>
+    </div>
+  </div>
+  <body>
+    
+  <footer>
+    <div class="footerContent">
+      <div class="logoWrapper">
+        <img class="logo" src="images/download.png" border="0" />
+      </div>
+      <div class="leftFooter">
+        <h1 id="followUs">Follow Me on. . </h1>
+        <div class="iconWrapper">
+          <a href="https://www.facebook.com/">
+            <img class="socialIcon" src="images/facebook.png" border="0" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img class="socialIcon" src="images/ig.jpg" border="0" />
+          </a>
+          <a href="https://twitter.com/">
+            <img class="socialIcon" src="images/twitter.png" border="0" />
+          </a>
+        </div>
+      </div>
+      <div class="rightFooter">
+        <div class="memberCredit">
+          <h1>Created by</h1>
+        </div>
+        <div class="nameList">
+          <ul id="name">
+            <li>Otieno Amba</li>
+           
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  </body>
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>laundry Services</title>
+    <link rel="stylesheet" type="text/css" href="stylesbackup1.css">
+    <link rel="shortcut icon" href="images/download.png"/>
+
+  </head>
+  <div class="header">
+    <div class="logoWrapper">
+      <img class="logo" src="images/download.png" border="0" />
+     
+    </div>
+    <div class="motto">
+      <h1>We offer the most conveninent and proffessional way to laundry.</h1>
+    </div>
+  </div>
+  <body>
+    <div class="row">
+    <div class="content">
+
+      <div class="boxWrapper high400">
+        <div class="withBorder">
+          <a href="usermanagement.php">
+            <div class="welcome User">
+              <h2>If your are user</h2>
+            </div>
+          </a>
+          <a href="management.php">
+            <div class="welcome Staff">
+              <h2>If your are Staff/Admin</h2>
+            </div>
+          </a>
+      </div>
+    </div>
+
+    </div>
+  </div>
+
+  <footer>
+    <div class="footerContent">
+      <div class="logoWrapper">
+        <img class="logo" src="images/download.png" border="0" />
+      </div>
+      <div class="leftFooter">
+        <h1 id="followUs">Follow Me on. . </h1>
+        <div class="iconWrapper">
+          <a href="https://www.facebook.com/">
+            <img class="socialIcon" src="images/facebook.png" border="0" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img class="socialIcon" src="images/ig.jpg" border="0" />
+          </a>
+          <a href="https://twitter.com/">
+            <img class="socialIcon" src="images/twitter.png" border="0" />
+          </a>
+        </div>
+      </div>
+      <div class="rightFooter">
+        <div class="memberCredit">
+          <h1>Created by</h1>
+        </div>
+        <div class="nameList">
+          <ul id="name">
+            <li>Otieno Amba</li>
+           
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  </body>
+
+</html>
